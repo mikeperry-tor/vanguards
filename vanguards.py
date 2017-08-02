207e591c887fc614b681159f74aca877d2e8a2e9
Mike Perry <mikeperry-git@torproject.org>
2017-08-01 23:09:57 -0400

    Implement Layer2 and Layer3 selection.

--- vanguards.py
+++ vanguards.py
@@ -6,23 +6,50 @@ import logging
 import copy
 import random
 import os
+import time
 
 import stem
 import stem.connection
 import stem.descriptor
 from stem.control import Controller
 
+import argparse
+import pickle
+
+NUM_LAYER1_GUARDS = 0 # Use Tor default
+NUM_LAYER2_GUARDS = 3
+NUM_LAYER3_GUARDS = 4
+
+# In days:
+LAYER1_LIFETIME = 0 # Use tor default
+
+# Hours
+MIN_LAYER2_LIFETIME = 24*1
+MAX_LAYER2_LIFETIME = 24*32
+
+# Hours
+MIN_LAYER3_LIFETIME = 1
+MAX_LAYER3_LIFETIME = 18
+
+SEC_PER_HOUR = (60*60)
+
 # MVP Plan:
 # 1. Get consensus
 # 2. Pass router list through Vanguard node filter/generator
-# 3. Choose N,M Vanguards by bandwidth * Wmm
-# 4. SETCONF torrc
+# 3. Options:
+#    a. Provide client and server options
+#    b. Provide num_guards options
+#    c. Choose N,M Vanguards by bandwidth * Wmm
+# 4. GETCONF/SETCONF torrc
+# 5. Implement rotation/expiry
+# 6. Subscribe to consensus events & follow prop271 for down nodes..
+# 7. Percentile restrictions? MTBF restrictions?
 
-# Later:
-# 0. Provide client and server options
-# 1. Log circuit construction and use
-# 2. Verify it follows our vanguard settings
-# 3. Subscribe to consensus events & follow prop271 for down nodes..
+# Experimentation:
+# 1. Onionperf scripts
+# 2. Log circuit paths; verify proper path restrictions
+# 3. Log CBT learning and timeout rate
+# 4. Tools to audit+verify it follows our vanguard settings
 
 logger = None
 loglevel = "DEBUG"
@@ -256,13 +283,16 @@ class BwWeightedGenerator(NodeGenerator):
       yield self.rstr_routers[choice_idx-1]
 
 
-def build_sorted_rlist(controller):
+def get_rlist_and_rdict(controller):
   sorted_r = list(controller.get_network_statuses())
+  dict_r = {}
   sorted_r.sort(lambda x, y: cmp(y.measured, x.measured))
 
   for i in xrange(len(sorted_r)): sorted_r[i].list_rank = i
 
-  return sorted_r
+  for r in sorted_r: dict_r[r.fingerprint] = r
+
+  return (sorted_r, dict_r)
 
 def connect():
   try:
@@ -299,18 +329,217 @@ def get_consensus_weights(controller):
   assert(parsed_consensus.is_consensus)
   return parsed_consensus.bandwidth_weights
 
+def setup_options():
+  global LAYER1_LIFETIME
+  global MIN_LAYER2_LIFETIME, MAX_LAYER2_LIFETIME
+  global MIN_LAYER3_LIFETIME, MAX_LAYER3_LIFETIME
+  global NUM_LAYER1_GUARDS
+  global NUM_LAYER2_GUARDS
+  global NUM_LAYER3_GUARDS
+
+  parser = argparse.ArgumentParser()
+  parser.add_argument("--num_guards", type=int, dest="num_layer1",
+                    help="Number of entry gurds (default "+str(NUM_LAYER1_GUARDS)+
+                    "; 0 means use tor's value)", default=NUM_LAYER1_GUARDS)
+
+  parser.add_argument("--num_mids", type=int, dest="num_layer2",
+                    help="Number of Layer2 guards (default "+str(NUM_LAYER2_GUARDS)+
+                    "; 0 disables)",
+                    default=NUM_LAYER2_GUARDS)
+
+  parser.add_argument("--num_ends", type=int, dest="num_layer3",
+                    default=NUM_LAYER3_GUARDS,
+                    help="Number of Layer3 guards (default "+str(NUM_LAYER3_GUARDS)+
+                    "; 0 disables)")
+
+  parser.add_argument("--guard_lifetime", type=int, dest="guard_lifetime",
+                    default=LAYER1_LIFETIME,
+                    help="Lifetime of Layer1 in days (default "+str(LAYER1_LIFETIME)+
+                    "; 0 means use tor's value)")
+
+  parser.add_argument("--mid_lifetime_min", type=int, dest="mid_lifetime_min",
+                    default=MIN_LAYER2_LIFETIME,
+                    help="Min lifetime of Layer2 in hours (default "+str(MIN_LAYER2_LIFETIME)+
+                    ")")
+
+  parser.add_argument("--mid_lifetime_max", type=int, dest="mid_lifetime_max",
+                    default=MAX_LAYER2_LIFETIME,
+                    help="Max lifetime of Layer2 in hours (default "+str(MAX_LAYER2_LIFETIME)+
+                    ")")
+
+  parser.add_argument("--end_lifetime_min", type=int, dest="end_lifetime_min",
+                    default=MIN_LAYER3_LIFETIME,
+                    help="Min lifetime of Layer3 in hours (default "+str(MIN_LAYER3_LIFETIME)+
+                    ")")
+
+  parser.add_argument("--end_lifetime_max", type=int, dest="end_lifetime_max",
+                    default=MAX_LAYER3_LIFETIME,
+                    help="Max lifetime of Layer3 in hours (default "+str(MAX_LAYER3_LIFETIME)+
+                    ")")
+
+  parser.add_argument("--state_file", dest="state_file", default="vanguards",
+                    help="File to store vanguard state (default: DataDirectory/vanguards)")
+
+  options = parser.parse_args()
+
+  (LAYER1_LIFETIME, MIN_LAYER2_LIFETIME, MAX_LAYER2_LIFETIME,
+   MIN_LAYER3_LIFETIME, MAX_LAYER3_LIFETIME, NUM_LAYER1_GUARDS,
+   NUM_LAYER2_GUARDS, NUM_LAYER3_GUARDS) = (options.guard_lifetime,
+   options.mid_lifetime_min, options.mid_lifetime_max,
+   options.end_lifetime_min, options.end_lifetime_max,
+   options.num_layer1, options.num_layer2, options.num_layer3)
+
+  return options
+
+class GuardNode:
+  def __init__(self, idhex, chosen_at, expires_at, priority):
+    self.idhex = idhex
+    self.chosen_at = chosen_at
+    self.expires_at = expires_at
+    self.priority_index = priority
+
+class VanguardState:
+  def __init__(self):
+    self.layer2 = []
+    self.layer2_down = []
+    self.layer3 = []
+    self.layer3_down = []
+
+  def write_to_file(self, outfile):
+    return pickle.dump(self, outfile)
+
+  @staticmethod
+  def read_from_file(infile):
+    return pickle.load(infile)
+
+  def layer2_guardset(self):
+    return ",".join(map(lambda g: g.idhex, self.layer2))
+
+  def layer3_guardset(self):
+    return ",".join(map(lambda g: g.idhex, self.layer3))
+
+  # Adds a new layer2 guard
+  def add_new_layer2(self, generator, priority):
+    guard = generator.next()
+    now = time.time()
+    expires = now + min(random.uniform(MIN_LAYER2_LIFETIME*SEC_PER_HOUR,
+                                       MAX_LAYER2_LIFETIME*SEC_PER_HOUR),
+                        random.uniform(MIN_LAYER2_LIFETIME*SEC_PER_HOUR,
+                                       MAX_LAYER2_LIFETIME*SEC_PER_HOUR))
+    self.layer2.append(GuardNode(guard.fingerprint, now, expires, priority))
+
+  def add_new_layer3(self, generator, priority):
+    guard = generator.next()
+    now = time.time()
+    expires = now + max(random.uniform(MIN_LAYER3_LIFETIME*SEC_PER_HOUR,
+                                       MAX_LAYER3_LIFETIME*SEC_PER_HOUR),
+                        random.uniform(MIN_LAYER3_LIFETIME*SEC_PER_HOUR,
+                                       MAX_LAYER3_LIFETIME*SEC_PER_HOUR))
+    self.layer3.append(GuardNode(guard.fingerprint, now, expires, priority))
+
+  def _remove_expired(self, remove_from, now):
+    for g in list(remove_from):
+      if g.expires_at < now:
+        remove_from.remove(g)
+
+  def replace_expired(self, generator):
+    now = time.time()
+
+    self._remove_expired(self.layer2, now)
+    self.layer2 = self.layer2[:NUM_LAYER2_GUARDS]
+    self._remove_expired(self.layer3, now)
+    self.layer3 = self.layer3[:NUM_LAYER2_GUARDS]
+    self._remove_expired(self.layer2_down, now)
+    self._remove_expired(self.layer3_down, now)
+
+    while len(self.layer2) < NUM_LAYER2_GUARDS:
+      self.add_new_layer2(generator, 0)
+
+    while len(self.layer3) < NUM_LAYER3_GUARDS:
+      self.add_new_layer3(generator, 0)
+
+  def _remove_down(self, remove_from, dict_r):
+    removed = []
+    for g in list(remove_from):
+      if not g.idhex in dict_r:
+        remove_from.remove(g)
+        removed.append(g)
+    return removed
+
+  def consensus_update(self, dict_r, generator):
+    # If any guards are down, move them from current to down
+    self.layer2_down.extend(self._remove_down(self.layer2, dict_r))
+    self.layer3_down.extend(self._remove_down(self.layer3, dict_r))
+
+    # If we drop below our target, first check for re-upped guards,
+    # then if none, add more with lower priority
+    # (Yeah, this is suboptimal, but it is not critical)
+    while len(self.layer2) < NUM_LAYER2_GUARDS:
+      min_up = None
+      for g in self.layer2_down:
+        if g.idhex in dict_r:
+          if not min_up or min_up.priority_index > g.priority_index:
+            min_up = g
+
+      # XXX: +1 over current highest priority?
+      if not min_up: self.add_new_layer2(generator, 1)
+      else:
+        self.layer2.append(min_up)
+        self.layer2_down.remove(min_up)
+
+    while len(self.layer3) < NUM_LAYER3_GUARDS:
+      min_up = None
+      for g in self.layer3_down:
+        if g.idhex in dict_r:
+          if not min_up or min_up.priority_index > g.priority_index:
+            min_up = g
+
+      # XXX: +1 over current highest priority?
+      if not min_up: self.add_new_layer3(generator, 1)
+      else:
+        self.layer3.append(min_up)
+        self.layer3_down.remove(min_up)
+
+def configure_tor(controller, vanguard_state):
+  if NUM_LAYER1_GUARDS:
+    controller.set_conf("NumEntryGuards", NUM_LAYER1_GUARDS)
+
+  if LAYER1_LIFETIME:
+    controller.set_conf("GuardLifetime", str(LAYER1_LIFETIME)+" days")
+
+  controller.set_conf("HSLayer2Guards", vanguard_state.layer2_guardset())
+  controller.set_conf("HSLayer3Guards", vanguard_state.layer3_guardset())
+
+  controller.save_conf()
+
 def main():
+  options = setup_options()
+
   controller = connect()
-  sorted_r = build_sorted_rlist(controller)
+  (sorted_r, dict_r) = get_rlist_and_rdict(controller)
   weights = get_consensus_weights(controller)
 
   ng = BwWeightedGenerator(sorted_r,
                      NodeRestrictionList([FlagsRestriction(["Fast", "Stable", "Guard"],
                                                            [])]),
                            weights, BwWeightedGenerator.POSITION_MIDDLE)
+  gen = ng.generate()
 
-  for r in ng.generate():
-    print r.nickname
+  try:
+    f = open(options.state_file)
+    state = VanguardState.read_from_file(f)
+  except:
+    state = VanguardState()
+
+  state.consensus_update(dict_r, gen)
+  state.replace_expired(gen)
+
+  configure_tor(controller, state)
+
+  state.write_to_file(open(options.state_file, "w"))
+
+  # XXX: Monitor for new consensus events...
+  # TODO: Monitor+log circuit construction and usage
 
 if __name__ == '__main__':
   main()
