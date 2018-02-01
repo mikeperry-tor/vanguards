a7f407b8bf2f28bed1c9c546c9aebceb28af1426
Mike Perry <mikeperry-git@torproject.org>
2018-02-01 05:30:43 +0000

    Setuptools + requirements.txt.

---
+++ setup.py
@@ -0,0 +1,49 @@
+"""setup.py: setuptools control."""
+
+import io
+import os
+
+from setuptools import setup
+
+# Read version and other info from package's __init.py file
+module_info = {}
+init_path = os.path.join(os.path.dirname(__file__), 'vanguards',
+                         '__init__.py')
+with open(init_path) as init_file:
+    exec(init_file.read(), module_info)
+
+
+def read(*names, **kwargs):
+    return io.open(
+        os.path.join(os.path.dirname(__file__), *names),
+        encoding=kwargs.get("encoding", "utf8")
+    ).read()
+
+setup(
+    name="vanguards",
+    packages=["vanguards"],
+    entry_points={
+        "console_scripts": [
+            'vanguards = vanguards.vanguards:main',
+        ]},
+    description="Vanguards help guard you from getting vanned...",
+    long_description=read('README.md'),
+    version=module_info.get('__version__'),
+    author=module_info.get('__author__'),
+    author_email=module_info.get('__contact__'),
+    url=module_info.get('__url__'),
+    license=module_info.get('__license__'),
+    keywords='tor',
+    install_requires=[
+        'setuptools',
+        'stem>=1.2.2',
+        ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License (MIT)',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+    ]
+)
--- vanguards/__init__.py
+++ vanguards/__init__.py
@@ -2,4 +2,5 @@
 
 __version__ = "0.0.0"
 __author__ = "Mike Perry"
-__license__ = "MIT"
+__license__ = "MIT/Expat"
+__url__ = "https://github.com/mikeperry-tor/vanguards"
