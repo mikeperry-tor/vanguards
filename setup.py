abf16b7b4758439330d91bd1cafbf02ea9553156
Mike Perry <mikeperry-git@torproject.org>
2018-07-20 01:01:20 +0000

    Fix the license classifier string.

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ setup(
         ],
     classifiers=[
         'Development Status :: 4 - Beta',
-        'License :: OSI Approved :: MIT License (MIT)',
+        'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
