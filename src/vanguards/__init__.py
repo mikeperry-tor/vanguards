a821906d397791ed55543d51c6cacfff9ae76f30
Mike Perry <mikeperry-git@torproject.org>
2018-08-07 23:31:00 +0000

    Bump version.

--- src/vanguards/__init__.py
+++ src/vanguards/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-__version__ = "0.1.1"
+__version__ = "0.2.0.dev0"
 __author__ = "Mike Perry"
 __license__ = "MIT/Expat"
 __url__ = "https://github.com/mikeperry-tor/vanguards"
