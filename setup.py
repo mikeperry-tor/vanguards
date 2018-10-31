e5482b78719121a92edb5721cf594359ff8b7feb
Mike Perry <mikeperry-git@torproject.org>
2018-10-31 21:05:57 +0000

    Fix setup.py and test-requirements.txt versions.

--- setup.py
+++ setup.py
@@ -69,7 +69,7 @@ setup(
     install_requires=[
         'setuptools',
         'ipaddress>=1.0.17 ; python_version<"3"',
-        'stem==1.5.4',
+        'stem==1.7.0',
         ],
     classifiers=[
         'Development Status :: 4 - Beta',
